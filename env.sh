76e9a67ad3b98df0ad32a5be91d83efc9efdc750
chenyaofo <chenyaofo@gmail.com>
2019-05-06 17:03:35 +0800

    Update env.sh

--- env.sh
+++ env.sh
@@ -2,8 +2,9 @@ rm -rf ~/miniconda3.sh && \
 rm -rf ~/miniconda3 && \
 wget https://mirrors.cloud.tencent.com/anaconda/miniconda/Miniconda3-4.5.12-Linux-x86_64.sh -O ~/miniconda3.sh && \
 /bin/bash ~/miniconda3.sh -b -p ~/miniconda3 && \
-~/miniconda3/bin/pip install --upgrade -i https://pypi.tuna.tsinghua.edu.cn/simple pip && \
-~/miniconda3/bin/pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple && \
+rm -rf ~/miniconda3.sh && \
+~/miniconda3/bin/pip install --upgrade -i https://mirrors.cloud.tencent.com/pypi/simple pip && \
+~/miniconda3/bin/pip config set global.index-url https://mirrors.cloud.tencent.com/pypi/simple && \
 ~/miniconda3/bin/pip install torch torchvision && \
 ~/miniconda3/bin/pip install ipython pyhocon future tb-nightly && \
 ~/miniconda3/bin/pip install git+https://github.com/chenyaofo/flame.git@master && \
