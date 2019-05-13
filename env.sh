24311733fd4a490f0150fb7ca9d7ad112cd8c90f
chenyaofo <chenyaofo@gmail.com>
2019-05-13 16:26:30 +0800

    Update env.sh

--- env.sh
+++ env.sh
@@ -3,7 +3,7 @@ rm -rf ~/miniconda3 && \
 proxychains4 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3.sh && \
 /bin/bash ~/miniconda3.sh -b -p ~/miniconda3 && \
 rm -rf ~/miniconda3.sh && \
-~/miniconda3/bin/pip config set global.index-url https://mirrors.cloud.tencent.com/pypi/simple && \
+~/miniconda3/bin/pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple && \
 ~/miniconda3/bin/pip install torch torchvision && \
 ~/miniconda3/bin/pip install ipython pyhocon future tb-nightly && \
 ~/miniconda3/bin/pip install git+https://github.com/chenyaofo/flame.git@master && \
