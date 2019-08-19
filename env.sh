97a6eca5a5c67c6bd5930faa55ee15872185a031
chenyaofo <chenyaofo@gmail.com>
2019-08-19 17:29:35 +0800

    Update env.sh

--- env.sh
+++ env.sh
@@ -4,8 +4,9 @@ wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/miniconda/Miniconda3-latest-L
 /bin/bash ~/miniconda3.sh -b -p ~/miniconda3 && \
 rm -rf ~/miniconda3.sh && \
 ~/miniconda3/bin/pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple && \
+~/miniconda3/bin/pip install cython future && \
 ~/miniconda3/bin/pip install torch torchvision && \
-~/miniconda3/bin/pip install future numpy scipy pandas matplotlib && \
+~/miniconda3/bin/pip install numpy scipy pandas matplotlib && \
 ~/miniconda3/bin/pip install ipython pyhocon tb-nightly pycocotools  && \
 ~/miniconda3/bin/pip install git+https://github.com/chenyaofo/flame.git@master && \
 echo '    ______        _                ______          ___                __' && \
