af01687100b7b6412c615816d8eedfbcf77da38a
chenyaofo <chenyaofo@gmail.com>
2019-09-26 06:03:56 -0700

    update

--- env-cn.sh
+++ env-cn.sh
@@ -13,6 +13,8 @@ rm -rf ~/miniconda3.sh && \
 ~/miniconda3/bin/conda install -y setuptools cython future ipython -c https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main && \
 ~/miniconda3/bin/conda install -y pytest graphviz jinja2 -c https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main && \
 ~/miniconda3/bin/conda install -y ipdb pyhocon glances -c https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/conda-forge && \
+# image library
+~/miniconda3/bin/conda install -y accimage pillow -c https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/conda-forge && \
 # development
 ~/miniconda3/bin/conda install -y pylint autopep8 mypy flake8 yapf black -c https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/conda-forge && \
 # science computation
@@ -21,6 +23,8 @@ rm -rf ~/miniconda3.sh && \
 ~/miniconda3/bin/conda install -y pytorch torchvision cudatoolkit=10.0 -c https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/pytorch && \
 # clean the caches
 ~/miniconda3/bin/conda clean -ay && \
+# conda init
+~/miniconda3/bin/conda init bash zsh && \
 # set pip mirror
 ~/miniconda3/bin/pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple && \
 # standalone tensorboard, only available on pypi
